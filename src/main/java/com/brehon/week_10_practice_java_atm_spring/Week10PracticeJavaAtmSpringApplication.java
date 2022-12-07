3f0c2126e663286fd46c5a22d36304faec4f3f48
= <=>
2022-12-07 12:12:00 +0330

    create project

---
+++ src/main/java/com/brehon/week_10_practice_java_atm_spring/Week10PracticeJavaAtmSpringApplication.java
@@ -0,0 +1,13 @@
+package com.brehon.week_10_practice_java_atm_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10PracticeJavaAtmSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week10PracticeJavaAtmSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brehon/week_10_practice_java_atm_spring/Week10PracticeJavaAtmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brehon.week_10_practice_java_atm_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10PracticeJavaAtmSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
